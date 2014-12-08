dfdb4206bcc7e5c541ea8f4007d477cc9041099f
satishbhat-umd <bhat.satish7@gmail.com>
2014-12-08 16:04:32 -0500

    Update project7.c

--- project7.c
+++ project7.c
@@ -38,28 +38,27 @@ int main(int argc, char *argv[]) {
    Array *pineapple = malloc(sizeof(Array));
    int left_over = atoi(argv[1]) % atoi(argv[2]);
    int i = 0;
-   pthread_t *threads = calloc(argv[2], sizeof(pthread_t));
-   pineapple -> random_ints = calloc(argv[1], sizeof(int));
+   pthread_t *threads = calloc(atoi(argv[2]), sizeof(pthread_t));
+   pineapple -> random_ints = calloc(atoi(argv[1]), sizeof(int));
    pineapple -> length = atoi(argv[1]) / atoi(argv[2]);
    sem_init(&mutex, 0, 1);
 
    srand(atoi(argv[3]));
 
-   for (i = 0; i < *argv[1]; i++) {
+   for (i = 0; i < atoi(argv[1]); i++) {
       pineapple -> random_ints[i] = rand();
    }
-   if (*argv[4] == 1) {
-      for (i = 0; i < *(argv[2]) - 1; i) {
+   if (atoi(argv[4]) == 1) {
+      for (i = 0; i < atoi(argv[2]) - 1; i++) {
          pthread_create(&threads[i], NULL, get_max, pineapple);
       }
       pineapple -> length += left_over;
       pthread_create(&threads[i], NULL, get_max, pineapple);
-      for (i = 0; i < *(argv[2]) - 1; i) {
+      for (i = 0; i < atoi(argv[2]) - 1; i++) {
          pthread_join(threads[i], NULL);
 
       }
       printf("MAX %d\n", pineapple->MAX);
-
    } else {
       /* SUM */
    }

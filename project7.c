8359582ae8eac4de41ea80a369c9648309df803a
Jgomez13 <jgomez13@terpmail.umd.edu>
2014-12-08 18:39:10 -0500

    Update project7.c

--- project7.c
+++ project7.c
@@ -5,25 +5,36 @@
 #include <semaphore.h>
 
 static int SUM = 0;
-static sem_t mutex;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct array {
-   int length;
-   int MAX;
-   int *random_ints;
+   int threads;/*number of threads*/
+   int left_over;/*if the #of ele doesnt divide evenly we neet to account*/
+   int count; /*which current thread*/
+   int length; /*the size of the current threads array*/
+   int MAX; /*the Max*/
+   int *random_ints; /*the array of random integers*/
 } Array;
 
 void *get_max(void *args) {
+   Array *p = (Array *) args;
    int i = 0;
-   Array *p = (Array *)args;
-   p->MAX = 0;
+
+   pthread_mutex_lock(&mutex);
    for (i = 0; i < p->length; i++) {
-      sem_wait(&mutex);
       if (p->random_ints[i] > p->MAX) {
          p->MAX = p->random_ints[i];
       }
-      sem_post(&mutex);
    }
+   p->random_ints += p->length;
+   p->count++;
+   /*when the current thread is the last one*/
+   if (p->count == (p->threads - 1)) {
+      /*the last thread will have the left over elements*/
+      p->length += p->left_over;
+   }
+   pthread_mutex_unlock(&mutex);
+   p = NULL;
    return NULL;
 }
 
@@ -36,47 +47,52 @@ int main(int argc, char *argv[]) {
       5- print
 
     */
-   Array *pineapple = malloc(sizeof(Array)) ;
-    
-   int elements = atoi(argv[1]), num_threads = atoi(argv[2]), left_over =
-      elements % num_threads, seed = atoi(argv[3]), task = atoi(argv[4]), i = 0, maxh=-1;
-
+   Array *pineapple = calloc(1,sizeof(Array));
+   int *restore, elements = atoi(argv[1]), num_threads = atoi(argv[2]), seed =
+      atoi(argv[3]), task = atoi(argv[4]), i = 0, expected = 0;
+   char print = *argv[5];
    pthread_t *threads = calloc(num_threads, sizeof(pthread_t));
 
    pineapple->random_ints = calloc(elements, sizeof(int));
    pineapple->length = elements / num_threads;
-   pineapple->random_ints= calloc(1, sizeof(Array));
-   sem_init(&mutex, 0, 1);
+   pineapple->MAX = 0;
+   pineapple->count = 0;
+   pineapple->threads = num_threads;
+   pineapple->left_over = elements % num_threads;
+   /*use restore to free because i move original array*/
+   restore = pineapple->random_ints;
 
    srand(seed);
 
    for (i = 0; i < elements; i++) {
-      int random = rand() % 100;
-      if (random > maxh){
-         maxh = random;
-      }
+      int random = rand();
       pineapple->random_ints[i] = random;
-      printf("random %d\n", random);
+      if (random > expected) {
+         expected = random;
+      }
    }
-   printf("maxh :%d\n", maxh);
+
    if (task == 1) {
        /*MAX*/ 
+      pthread_mutex_init(&mutex, NULL);
       for (i = 0; i < num_threads; i++) {
-         if(i == (num_threads - 2)) {
-             pineapple->length += left_over;
-         }
-          pthread_create(&threads[i], NULL, get_max, pineapple);
+         pthread_create(&threads[i], NULL, get_max, pineapple);
       }
       for (i = 0; i < num_threads; i++) {
          pthread_join(threads[i], NULL);
       }
-      printf("MAX %d\n", pineapple->MAX);
-
+      pthread_mutex_destroy(&mutex);
+      if(print == 'Y') {
+         printf("%d\t our_MAX\n%d\t expected\n", pineapple->MAX, expected);
+      }
    } else {
       /* SUM */
       SUM = 0;
    }
-   free(pineapple-> random_ints);
+   free(restore);
+   restore = NULL;
+   pineapple->random_ints = NULL;
    free(pineapple);
+   pineapple = NULL;
    return 0;
 }
